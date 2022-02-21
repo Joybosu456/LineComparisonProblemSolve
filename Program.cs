a3ae36aba38c6dba8c759f75cb2d05c4cb7ed0da
JOY <joybosu@gmail.com>
2022-02-21 12:14:49 +0530

    Add line comparison problem

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparisonProb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            LengthCalculate.CartLengthCalculate();
+        }
+    }
+}
